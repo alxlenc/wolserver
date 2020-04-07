b02808ee854fd65193a81f79da4821b761384b44
alxlenc <1325588+alxlenc@users.noreply.github.com>
2020-04-07 20:12:21 +0200

    add wol functionality

---
+++ src/main/java/org/alxlenc/wolserver/WolserverApplication.java
@@ -0,0 +1,13 @@
+package org.alxlenc.wolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alxlenc/wolserver/WolserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alxlenc.wolserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
